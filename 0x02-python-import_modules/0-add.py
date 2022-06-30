04680fef838b47fe20e0b43621aa8c1ed905403d
Maranata <maranataflk9@gmail.com>
2022-06-30 11:00:38 -0700

    python new

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     import add_o as add
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add.add(a, b)))
+    print("{:d} + {:d} = {:d} \n".format(a, b, add.add(a, b)))
