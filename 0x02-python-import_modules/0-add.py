3967d76e3a2e0d764996024ac19584ff165005b9
Maranata <maranataflk9@gmail.com>
2022-06-30 10:44:56 -0700

    python

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import add_o as add
+a = 1
+b = 2
+print(str(a) + " + " + str(b) + " = " + str(add.add(a, b)) + "\n")
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+This directory includes tasks for 0x02-python-import_modules
