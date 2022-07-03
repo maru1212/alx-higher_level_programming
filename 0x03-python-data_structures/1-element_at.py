36c3cace2436bbf79ae58aaeea25e7478acf6135
Maranata <maranataflk9@gmail.com>
2022-07-03 08:03:14 -0700

    python

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(mylist, idx):
-    if idx >= 0 and len(mylist) >= idx:
+    if idx >= 0 and len(mylist) > idx:
         return(mylist[idx])
     else:
         return("None")
