0a3dacf4abfc45056543b001790b48452024b0f7
Maranata <maranataflk9@gmail.com>
2022-07-04 23:51:07 -0700

    c palindrom

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * main - check the code for palidrome or not.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+
+	return(0);
+}
