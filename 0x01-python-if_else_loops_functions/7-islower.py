a4aba8a07eef867b11de00a77e44cbce33fd0b45
Maranata <maranataflk9@gmail.com>
2022-07-16 11:10:00 -0700

    python

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def islower(c):
-    for alpha in range(ord('a'), ord('z') + 1):
-        if c == chr(alpha):
-            return(True)
-        elif c != chr(alpha) and chr(alpha) == "z":
-            return(False)
+    if type(c) == str:
+        for alpha in range(ord('a'), ord('z') + 1):
+            if c == chr(alpha):
+                return(True)
+            elif c != chr(alpha) and chr(alpha) == "z":
+                return(False)
