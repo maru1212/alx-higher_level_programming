93e0306123d222822031abe2651e70f5fc244658
Maranata <maranataflk9@gmail.com>
2022-07-19 12:33:06 -0700

    python class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
+"""Define a class square."""
+
 class Square:
     """Represent a square."""
-
-    def __init__(self):
-        """Initialize a new square."""
-        pass
+    pass
