e914f0d580eabe0e22b860f7d848dff284c6bbda
Maranata <maranataflk9@gmail.com>
2022-07-20 12:55:52 -0700

    python

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,12 +47,9 @@ class Square:
 
     def my_print(self):
         """Return # squares."""
-        for i in range(0, self.__position[1]):
-            print("")
+        [print("") for i in range(0, self.__position[1])]
 
         for i in range(0, self.__size):
-            for j in range(0, self.__position[0]):
-                print(" ", end="")
-            for k in range(0, self.__size):
-                print("#", end="")
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
