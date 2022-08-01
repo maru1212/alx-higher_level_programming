5065ac74ac3001be5fadad7aedef9803580b4e2d
Maranata <maranataflk9@gmail.com>
2022-08-01 12:37:27 -0700

    python

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -13,6 +13,6 @@ def is_same_class(obj, a_class):
          otherwise - False.
     """
 
-    if isinstance(type(obj), a_class):
+    if type(obj) == a_class:
         return True
     return False
