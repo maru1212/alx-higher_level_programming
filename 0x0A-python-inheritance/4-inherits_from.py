3b779eb3642dbbc60621c7658f182e44c2f5819a
Maranata <maranataflk9@gmail.com>
2022-08-02 12:26:23 -0700

    python inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,17 +2,17 @@
 """ Function to check for an object in the same class. """
 
 
-def iherits_from(obj, a_class):
+def inherits_from(obj, a_class):
     """ A function to check for an object in a class.
 
     Args:
          obj(any): The object to be checked
          a_class(type): To be matched with the object
     Returns:
-         If the obj is instance of a class - True
+         If the obj is inherited instance of a class - True
          otherwise - False.
     """
 
-    if issubclass(obj, a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
