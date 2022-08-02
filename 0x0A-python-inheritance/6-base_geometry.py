591538bb3e1ca321916dec6e1c03c3b0f422aad9
Maranata <maranataflk9@gmail.com>
2022-08-02 12:38:42 -0700

    python exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Author: Maranata Feleke, an emtpy class. """
+
+
+class BaseGeometry():
+    """ This is an empty class. """
+
+    def area(self):
+        """ A function which raises an exception."""
+        raise Exception("area() is not implemebted")
