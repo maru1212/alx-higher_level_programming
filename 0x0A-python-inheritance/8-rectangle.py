5e632fc0253e47fae336bd4972e927ca9cbd89dd
Maranata <maranataflk9@gmail.com>
2022-08-04 10:10:43 -0700

    python inh

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """ Author: Maranata Feleke, an emtpy class. """
-
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """ This is an empty class. """
 
     def __init__(self, width, height):
         """ Instantiation of the attriibutes.
+
         Args:
-             width(int): width of the rectangle
-             height(int): height of the rectangle
+             width(int): width of the rectangle.
+             height(int): height of the rectangle.
         """
 
         self._width = width
         self._height = height
-
         self.integer_validator("width", width)
         self.integer_validator("height", height)
