dab2a4ab64af7735fd7603b94211cc1ca635537e
maru1212 <maranataflk9@gmail.com>
2022-08-09 11:33:13 -0700

    python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,8 +3,13 @@
 
 
 class Base:
-    """ A base class"""
+    """ A base class
+       private Attribute:
+           __nb_objects(int): private attribute.
+    """
+    
     __nb_objects = 0
+    
     def __init__(self, id=None):
         """Intiator
         Args:
