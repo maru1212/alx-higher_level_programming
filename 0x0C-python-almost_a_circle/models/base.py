09358005c9f2b42b4d46201661b70e533b4b921a
maru1212 <maranataflk9@gmail.com>
2022-08-09 11:23:48 -0700

    python

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A base class"""
+
+
+class Base:
+    """ A base class"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Intiator
+        Args:
+            id(int): unique id of an object.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
