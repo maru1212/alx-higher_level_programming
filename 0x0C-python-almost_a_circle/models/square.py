705fe44a7805b8bf0c55b21e5976c55cdfd2859a
maru1212 <maranataflk9@gmail.com>
2022-08-12 12:42:42 -0700

    python

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,42 @@ class Square(Rectangle):
     def __str__(self):
         return("{} ({}) {}/{} - {}".format(__class__.__name__, self.id,
                                            self.x, self.y, self.size))
+
+    def update(self, *args, **kwargs):
+        """update the new square.
+        Args:
+            args (int):
+                - 1st argument should be the id attribute.
+                - 2nd argument should be the size attribute.
+                - 3rd argument should be the x attribute.
+                - 4th argument should be the y attribute.
+            kwargs(key & value): includes a key and value for it.
+        """
+        if args and len(args) != 0:
+            inc = 0
+            for argument in args:
+                if inc == 0:
+                    if args is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = argument
+                elif inc == 1:
+                    self.size = argument
+                elif inc == 2:
+                    self.x = argument
+                elif inc == 3:
+                    self.y = argument
+                inc += 1
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
