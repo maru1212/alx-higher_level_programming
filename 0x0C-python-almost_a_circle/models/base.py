3d5cc819cd0cc33164de26b56468ab2d93de108d
maru1212 <maranataflk9@gmail.com>
2022-08-12 13:14:25 -0700

    python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,6 +23,7 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """ A static method which converts dictionary to json str. """
         if list_dictionaries is None:
             return []
         else:
