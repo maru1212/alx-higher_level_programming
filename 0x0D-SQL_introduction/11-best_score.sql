5ea641dbab01bf1c17fe3a6026b173f92bfc3d68
Maranata <maranataflk9@gmail.com>
2022-09-07 10:44:19 -0700

    filtering by score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- lists all records with score >= 10 in the table second_table
 SELECT score, name FROM second_table
-       where score >= 10 ORDER BY score DESC;
+WHERE score >= 10 ORDER BY score DESC;
