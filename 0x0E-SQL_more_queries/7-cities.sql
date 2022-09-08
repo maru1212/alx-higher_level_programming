82071b1fadd1a1c191ed521a9e622d21023d6752
Maranata <maranataflk9@gmail.com>
2022-09-08 13:16:45 -0700

    assigning a foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Creating a new database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Selecting the desired database
+USE hbtn_0d_usa;
+
+-- Creating a new table called cities.
+CREATE TABLE IF NOT EXISTS cities(
+       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       FOREIGN KEY (state_id) REFERENCES states(id));
