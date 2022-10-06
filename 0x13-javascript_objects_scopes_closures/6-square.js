cc21f74a9a0b139228e07b3f6c8c18152643aeb0
maru1212 <maranataflk9@gmail.com>
2022-10-06 17:48:02 +0000

    js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Square_ = require('./5-square');
+class Square extends Square_ {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        let result = '';
+        for (let j = 0; j < this.width; j++) {
+          result = result + 'C';
+        }
+        console.log(result);
+      }
+    } else {
+      super.print();
+    }
+  }
+}
+
+module.exports = Square;
